0d04835823c2ad15abe1e709d342f3a21de88d12
ATaylorC <ataylorcarroll08@gmail.com>
2025-01-25 12:27:29 -0700

    fixed CAN ID constant

--- Subsystem/Phoenix_motor.py
+++ Subsystem/Phoenix_motor.py
@@ -6,11 +6,12 @@ import wpilib
 import phoenix6
 import commands2
 
+from constants import ELEC
 
 class MotorSubsystem(commands2.Subsystem):
     def __init__(self):
         super().__init__()
-        self.phoenixmotor = phoenix6.hardware.TalonFX(1)
+        self.phoenixmotor = phoenix6.hardware.TalonFX(ELEC.Phoenix_motor_CAN_ID)
         self.controller = wpilib.XboxController
 
     def go_up(self):
@@ -20,7 +21,7 @@ class MotorSubsystem(commands2.Subsystem):
         self.phoenixmotor.set(-0.4)
 
     def stop(self):
-        self.motor.set(0)
+        self.phoenixmotor.set(0)
 
     def is_stopped(self):
         motor_speed = self.motor.get()
